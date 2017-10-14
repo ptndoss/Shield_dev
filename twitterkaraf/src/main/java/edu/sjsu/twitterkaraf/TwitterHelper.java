c0d1630cacefcee6b763a15f060e453cecb458cb
Shriaithal <shriaithal@gmail.com>
2017-10-13 23:08:13 -0700

    Adding test cases and comments

--- twitterkaraf/src/main/java/edu/sjsu/twitterkaraf/TwitterHelper.java
+++ twitterkaraf/src/main/java/edu/sjsu/twitterkaraf/TwitterHelper.java
@@ -6,9 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -24,17 +21,37 @@ import oauth.signpost.exception.OAuthCommunicationException;
 import oauth.signpost.exception.OAuthExpectationFailedException;
 import oauth.signpost.exception.OAuthMessageSignerException;
 
+/**
+ * Helper Class to invoke all Twitter APIs
+ * Uses signpost to make http calls.
+ * @author Anushri Srinath Aithal
+ * Nidhi Jamar
+ * Aswhwini Shankar Narayan
+ * Anuradha Rajashekar
+ *
+ */
 @SuppressWarnings({ "deprecation", "resource" })
 public class TwitterHelper {
-
+	
+	/**
+	 * Method to load the properties file while twitter tokens and api URLs are placed
+	 * @return Property File
+	 * @throws IOException
+	 * @author Anushri Srinath Aithal
+	 */
 	private Properties getProperties() throws IOException {
-		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-		InputStream input = classLoader.getResourceAsStream("/properties/application.properties");
+		InputStream input = this.getClass().getResourceAsStream("/properties/application.properties");
 		Properties properties = new Properties();
 		properties.load(input);
 		return properties;
 	}
 
+	/**
+	 * Create signpot OAuthConsumer to invoke Twitter APIs with right access tokens
+	 * @return OAuthConsumer
+	 * @throws IOException
+	 * @author Anushri Srinath Aithal
+	 */
 	public OAuthConsumer getOAuthConsumer() throws IOException {
 		Properties properties = getProperties();
 		String consumerKeyStr = properties.getProperty("twitter.consumer.key");
@@ -46,6 +63,17 @@ public class TwitterHelper {
 		return oAuthConsumer;
 	}
 
+	/**
+	 * Execute Http Post calls to invoke the post APIs
+	 * @param apiUrl
+	 * @return
+	 * @throws OAuthMessageSignerException
+	 * @throws OAuthExpectationFailedException
+	 * @throws OAuthCommunicationException
+	 * @throws IOException
+	 * @author Nidhi Jamar
+	 * @author Anuradha Rajashekar
+	 */
 	private HttpResponse executeHttpPost(String apiUrl) throws OAuthMessageSignerException,
 			OAuthExpectationFailedException, OAuthCommunicationException, IOException {
 		HttpPost httprequest = new HttpPost(apiUrl);
@@ -57,6 +85,17 @@ public class TwitterHelper {
 		return httpresponse;
 	}
 
+	/**
+	 * Execute Http Get calls to invoke the Get APIs
+	 * @param apiUrl
+	 * @return
+	 * @throws OAuthMessageSignerException
+	 * @throws OAuthExpectationFailedException
+	 * @throws OAuthCommunicationException
+	 * @throws IOException
+	 * @author Anushri Srinath Aithal
+	 * @author Ashwini Shankar Narayan
+	 */
 	private HttpResponse executeHttpGet(String apiUrl) throws OAuthMessageSignerException,
 			OAuthExpectationFailedException, OAuthCommunicationException, IOException {
 		HttpGet httprequest = new HttpGet(apiUrl);
@@ -68,80 +107,77 @@ public class TwitterHelper {
 		return httpresponse;
 	}
 
-	public void searchHashTag(HttpServletRequest request, HttpServletResponse response) {
+	/**
+	 * Implementing Twitter API to search tweets based on Hash Tag
+	 * @param searchHashTag
+	 * @return List of tweets, max of 10 records
+	 * @author Anushri Srinath Aithal
+	 */
+	public List<String> searchHashTag(String searchHashTag) {
+		List<String> twitterResponseList = new ArrayList<String>();
 		try {
 			Properties properties = getProperties();
 			String apiUrl = properties.getProperty("twitter.api.search.tweets");
-
-			String searchHashTag = request.getParameter("searchStr");
-			apiUrl = apiUrl + "?count=10&q=%23" + searchHashTag;
+			apiUrl = apiUrl + "?count=10&q=%23" + searchHashTag; // appending API url with search string
 
 			HttpResponse apiResponse = executeHttpGet(apiUrl);
 
 			if (200 == apiResponse.getStatusLine().getStatusCode()) {
-
-				List<String> twitterResponseList = new ArrayList<String>();
-
-				JSONObject jsonobject = new JSONObject(EntityUtils.toString(apiResponse.getEntity()));
+				JSONObject jsonobject = new JSONObject(EntityUtils.toString(apiResponse.getEntity())); //read json response
 				JSONArray jsonArray = (JSONArray) jsonobject.get("statuses");
 				for (int i = 0; i < jsonArray.length(); i++) {
 					JSONObject jsonObject = (JSONObject) jsonArray.get(i);
 					JSONObject userObject = (JSONObject) jsonObject.get("user");
-					String displayText = (String) userObject.get("screen_name") + " : "
-							+ (String) jsonObject.get("text");
-					twitterResponseList.add(displayText);
+					
+					String displayText = (String) userObject.get("screen_name") + " : " + (String) jsonObject.get("text");
+					twitterResponseList.add(displayText); //add tweet user name : tweet text
 				}
-
-				request.getSession().setAttribute("twitterResponse", twitterResponseList);
-				request.getSession().setAttribute("option", "Search Tweet");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
+		return twitterResponseList;
 	}
 
-	public void createFriendship(HttpServletRequest request, HttpServletResponse response) {
+	public List<String> createFriendship(String searchStr) {
+		List<String> twitterResponseList = new ArrayList<String>();
 		try {
 			Properties properties = getProperties();
 			String apiUrl = properties.getProperty("twitter.api.createfriendship");
-
-			String searchStr = request.getParameter("searchStr");
 			apiUrl = apiUrl + "?screen_name=" + searchStr;
 
 			HttpResponse apiResponse = executeHttpPost(apiUrl);
 
 			if (200 == apiResponse.getStatusLine().getStatusCode()) {
-				request.getSession().setAttribute("twitterResponse", "API call was Successful");
-				request.getSession().setAttribute("option", "Create Friendship");
+				twitterResponseList.add("API call was Successful");
 			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return twitterResponseList;
 	}
 
-	public void statusupdate(HttpServletRequest request, HttpServletResponse response) {
+	public List<String> statusUpdate(String searchStr) {
+		List<String> twitterResponseList = new ArrayList<String>();
 		try {
 			Properties properties = getProperties();
 			String apiUrl = properties.getProperty("twitter.api.status.update");
-
-			String searchStr = request.getParameter("searchStr");
 			apiUrl = apiUrl + "?status=" + searchStr;
 
 			HttpResponse apiResponse = executeHttpPost(apiUrl);
 
 			if (200 == apiResponse.getStatusLine().getStatusCode()) {
-				request.getSession().setAttribute("twitterResponse", "API call was Successful");
-				request.getSession().setAttribute("option", "Status Update");
+				twitterResponseList.add("API call was Successful");
 			}
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return twitterResponseList;
 	}
 
-	public void trendsavailable(HttpServletRequest request, HttpServletResponse response) {
+	public List<String> trendsAvailable() {
+		List<String> twitterResponseList = new ArrayList<String>();
 		try {
 			Properties properties = getProperties();
 			String apiUrl = properties.getProperty("twitter.api.trends.available");
@@ -149,25 +185,22 @@ public class TwitterHelper {
 			HttpResponse apiResponse = executeHttpGet(apiUrl);
 
 			if (200 == apiResponse.getStatusLine().getStatusCode()) {
-
-				List<String> twitterResponseList = new ArrayList<String>();
 				JSONArray jsonArray = new JSONArray(EntityUtils.toString(apiResponse.getEntity()));
-				for (int i = 0; i < jsonArray.length() || i == 10; i++) {
+				for (int i = 0; i < jsonArray.length() && i < 10; i++) {
 					JSONObject object = (JSONObject) jsonArray.get(0);
 					String displayText = (String) object.get("country") + " : " + (String) object.get("url");
 					twitterResponseList.add(displayText);
 				}
-
-				request.getSession().setAttribute("twitterResponse", twitterResponseList);
-				request.getSession().setAttribute("option", "Trends Available");
 			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return twitterResponseList;
 	}
 
-	public void languageSupport(HttpServletRequest request, HttpServletResponse response) {
+	public List<String> languageSupport() {
+		List<String> twitterResponseList = new ArrayList<String>();
 		try {
 			Properties properties = getProperties();
 			String apiUrl = properties.getProperty("twitter.api.language.support");
@@ -175,54 +208,51 @@ public class TwitterHelper {
 			HttpResponse apiResponse = executeHttpGet(apiUrl);
 
 			if (200 == apiResponse.getStatusLine().getStatusCode()) {
-
-				List<String> twitterResponseList = new ArrayList<String>();
 				JSONArray jsonArray = new JSONArray(EntityUtils.toString(apiResponse.getEntity()));
-				for (int i = 0; i < jsonArray.length() || i == 10; i++) {
+				for (int i = 0; i < jsonArray.length() && i < 10; i++) {
 					JSONObject object = (JSONObject) jsonArray.get(i);
 					String displayText = (String) object.get("name");
 					twitterResponseList.add(displayText);
 				}
-
-				request.getSession().setAttribute("twitterResponse", twitterResponseList);
-				request.getSession().setAttribute("option", "Language Support");
 			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return twitterResponseList;
 	}
 
-	public void homeTimeline(HttpServletRequest request, HttpServletResponse response) {
+	/**
+	 * Implementing twitter API to display tweets on home timeline
+	 * @return List of Tweets
+	 * @author Anushri Srinath Aithal
+	 */
+	public List<String> homeTimeline() {
+		List<String> twitterResponseList = new ArrayList<String>();
 		try {
 			Properties properties = getProperties();
-			String apiUrl = properties.getProperty("twitter.api.home.timeline") + "?count=10";
+			String apiUrl = properties.getProperty("twitter.api.home.timeline") + "?count=10"; //append count to max of 10
 
 			HttpResponse apiResponse = executeHttpGet(apiUrl);
 
 			if (200 == apiResponse.getStatusLine().getStatusCode()) {
-
-				List<String> twitterResponseList = new ArrayList<String>();
-
 				JSONArray jsonArray = new JSONArray(EntityUtils.toString(apiResponse.getEntity()));
 				for (int i = 0; i < jsonArray.length(); i++) {
 					JSONObject jsonObject = (JSONObject) jsonArray.get(i);
 					JSONObject userObject = (JSONObject) jsonObject.get("user");
-					String displayText = (String) userObject.get("screen_name") + " : "
-							+ (String) jsonObject.get("text");
-					twitterResponseList.add(displayText);
+					String displayText = (String) userObject.get("screen_name") + " : " + (String) jsonObject.get("text"); //data to be displayed in screen_name : tweet format
+					twitterResponseList.add(displayText); //list of data in the format mentioned above
 				}
-
-				request.getSession().setAttribute("twitterResponse", twitterResponseList);
-				request.getSession().setAttribute("option", "Home Timeline");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return twitterResponseList;
 
 	}
 
-	public void trendsClosest(HttpServletRequest request, HttpServletResponse response) {
+	public List<String> trendsClosest() {
+		List<String> twitterResponseList = new ArrayList<String>();
 		try {
 			Properties properties = getProperties();
 			String apiUrl = properties.getProperty("twitter.api.trends.closest") + "?lat=37.7749&long=122.4194";
@@ -230,49 +260,39 @@ public class TwitterHelper {
 			HttpResponse apiResponse = executeHttpGet(apiUrl);
 
 			if (200 == apiResponse.getStatusLine().getStatusCode()) {
-
-				List<String> twitterResponseList = new ArrayList<String>();
 				JSONArray jsonArray = new JSONArray(EntityUtils.toString(apiResponse.getEntity()));
-				for (int i = 0; i < jsonArray.length() || i == 10; i++) {
+				for (int i = 0; i < jsonArray.length() && i < 10; i++) {
 					JSONObject object = (JSONObject) jsonArray.get(i);
 					String displayText = (String) object.get("country") + " : " + (String) object.get("url");
 					twitterResponseList.add(displayText);
 				}
-
-				request.getSession().setAttribute("twitterResponse", twitterResponseList);
-				request.getSession().setAttribute("option", "Trends Closest");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
+		return twitterResponseList;
 	}
 
-	public void followersList(HttpServletRequest request, HttpServletResponse response) {
+	public List<String> followersList(String searchStr) {
+		List<String> twitterResponseList = new ArrayList<String>();
 		try {
 			Properties properties = getProperties();
-			String searchStr = request.getParameter("searchStr");
 			String apiUrl = properties.getProperty("twitter.api.follower.list") + "?screen_name=" + searchStr;
 
 			HttpResponse apiResponse = executeHttpGet(apiUrl);
 
 			if (200 == apiResponse.getStatusLine().getStatusCode()) {
-
-				List<String> twitterResponseList = new ArrayList<String>();
 				JSONObject jsonobject = new JSONObject(EntityUtils.toString(apiResponse.getEntity()));
 				JSONArray jsonArray = (JSONArray) jsonobject.get("users");
-				for (int i = 0; i < jsonArray.length() || i == 10; i++) {
+				for (int i = 0; i < jsonArray.length() && i < 10; i++) {
 					JSONObject object = (JSONObject) jsonArray.get(i);
 					String displayText = (String) object.get("name") + " : " + (String) object.get("screen_name");
 					twitterResponseList.add(displayText);
 				}
-
-				request.getSession().setAttribute("twitterResponse", twitterResponseList);
-				request.getSession().setAttribute("option", "Followers List");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
+		return twitterResponseList;
 	}
 }
--- twitterkaraf/src/main/java/edu/sjsu/twitterkaraf/TwitterServlet.java
+++ twitterkaraf/src/main/java/edu/sjsu/twitterkaraf/TwitterServlet.java
@@ -1,6 +1,8 @@
 package edu.sjsu.twitterkaraf;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,56 +10,74 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Servlet implementing Twitter APIs to be deployed on karaf
+ * @author Anushri Srinath Aithal
+ * Nidhi Jamar
+ * Aswhwini Shankar Narayan
+ * Anuradha Rajashekar
+ *
+ */
 public class TwitterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * servlet doPost method to implement all 8 Twitter APIs
+	 */
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		try {
 			TwitterHelper twitterHelper = new TwitterHelper();
 
 			String twitteroptions = request.getParameter("twitteroptions");
+			String searchStr = request.getParameter("searchStr");
+			List<String> twitterResponseList = new ArrayList<String>();
 
 			switch (twitteroptions) {
 			case "createfriendship":
-				twitterHelper.createFriendship(request, response);
+				twitterResponseList = twitterHelper.createFriendship(searchStr);
 				break;
 			case "statusupdate":
-				twitterHelper.statusupdate(request, response);
+				twitterResponseList = twitterHelper.statusUpdate(searchStr);
 				break;
 			case "trendsavailable":
-				twitterHelper.trendsavailable(request, response);
+				twitterResponseList = twitterHelper.trendsAvailable();
 				break;
 			case "searchHashTag":
-				twitterHelper.searchHashTag(request, response);
+				twitterResponseList = twitterHelper.searchHashTag(searchStr);
 				break;
 			case "homeTimeline":
-				twitterHelper.homeTimeline(request, response);
+				twitterResponseList = twitterHelper.homeTimeline();
 				break;
 			case "languageSupport":
-				twitterHelper.languageSupport(request, response);
+				twitterResponseList = twitterHelper.languageSupport();
 				break;
 			case "trendsClosest":
-				twitterHelper.trendsClosest(request, response);
+				twitterResponseList = twitterHelper.trendsClosest();
 				break;
 			case "followerList":
-				twitterHelper.followersList(request, response);
+				twitterResponseList = twitterHelper.followersList(searchStr);
 				break;
 
 			default:
 				break;
 
 			}
+			request.getSession().setAttribute("twitterResponse", twitterResponseList);
+			request.getSession().setAttribute("option", "twitteroptions");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/index.jsp");
 		requestDispatcher.forward(request, response);
 	}
 
+	/**
+	 * Servlet doGet method
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/index.jsp");
